418c6f4e19061cb47e38d15515510359d6144cdf
Gary Dziewior <gary.dziewior@sap.com>
2017-01-27 10:42:53 -0600

    Swapping logo JS removed.  IE tweaks

--- sites/all/modules/fieldglass/fieldglass.js
+++ sites/all/modules/fieldglass/fieldglass.js
@@ -7,16 +7,16 @@ jQuery( document ).ready(function() {
 
         jQuery(window).scroll(function() {
             var scroll = jQuery(window).scrollTop();
-            if (scroll >= 25) {
+            /*if (scroll >= 25) {
                 logo.attr("src","/sites/default/files/summit_logo_noring.png");
                 logo.addClass('logoswap');
                 /*logo.css('margin-top', '75px');
                 jQuery("div#region-menu-first").css("margin-top", "46");
-                jQuery("div#region-menu-second").css("margin-top", "47");*/
+                jQuery("div#region-menu-second").css("margin-top", "47");
             } else {
                 logo.attr("src","/sites/default/files/fgsummit_logo_rings.png");
                 logo.removeClass('logoswap');
-            }
+            }*/
             //Black Bar Stuff
             var block25 =jQuery('#block-block-25');
             var shouldBeVisible = jQuery(window).scrollTop()>50;
