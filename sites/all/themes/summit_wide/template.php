f05a496f21c5e20c414aaca5e5394aec76e8707e
Gary Dziewior <gary.dziewior@sap.com>
2017-07-06 10:12:34 -0500

    Added hook for changing logo url. Css for right menu

--- sites/all/themes/summit_wide/template.php
+++ sites/all/themes/summit_wide/template.php
@@ -10,3 +10,40 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+/**
+ * Implements hook_process_region().
+ */
+function summit_wide_process_region(&$vars) {
+    if (in_array($vars['elements']['#region'], array('content', 'menu', 'branding'))) {
+        $theme = alpha_get_theme();
+
+        switch ($vars['elements']['#region']) {
+            case 'content':
+                $vars['title_prefix'] = $theme->page['title_prefix'];
+                $vars['title'] = $theme->page['title'];
+                $vars['title_suffix'] = $theme->page['title_suffix'];
+                $vars['tabs'] = $theme->page['tabs'];
+                $vars['action_links'] = $theme->page['action_links'];
+                $vars['title_hidden'] = $theme->page['title_hidden'];
+                $vars['feed_icons'] = $theme->page['feed_icons'];
+                break;
+
+            case 'menu':
+                $vars['main_menu'] = $theme->page['main_menu'];
+                $vars['secondary_menu'] = $theme->page['secondary_menu'];
+                break;
+
+            case 'branding':
+                $vars['site_name'] = $theme->page['site_name'];
+                $vars['linked_site_name'] = l($vars['site_name'], '<front>', array('attributes' => array('title' => t('Home')), 'html' => TRUE));
+                $vars['site_slogan'] = $theme->page['site_slogan'];
+                $vars['site_name_hidden'] = $theme->page['site_name_hidden'];
+                $vars['site_slogan_hidden'] = $theme->page['site_slogan_hidden'];
+                $vars['logo'] = $theme->page['logo'];
+                $vars['logo_img'] = $vars['logo'] ? '<img src="' . $vars['logo'] . '" alt="' . check_plain($vars['site_name']) . '" id="logo" />' : '';
+                //changed to link "emea" for EMEA homepage. was front
+                $vars['linked_logo_img'] = $vars['logo'] ? l($vars['logo_img'], 'emea', array('attributes' => array('rel' => 'home', 'title' => check_plain($vars['site_name'])), 'html' => TRUE)) : '';
+                break;
+        }
+    }
+}
