21177cc4d44f037b469e31f86d13b1238d650243
Gary Dziewior <gary.dziewior@sap.com>
2017-08-01 09:44:02 -0500

    1st round tweaks - ginney

--- sites/all/modules/fieldglass/fieldglass.js
+++ sites/all/modules/fieldglass/fieldglass.js
@@ -4,15 +4,17 @@
 
         var $day1 = $("#views_slideshow_pager_field_item_top_agenda-block_1_0");
         var $day2 = $("#views_slideshow_pager_field_item_top_agenda-block_1_9");
-
+        var $desc = $(".field-name-field-agenda-session-description");
         $day1.click(function (){
             $day1.addClass("active");
             $day2.removeClass("active");
+            $desc.hide();
         });
 
         $day2.click(function (){
             $day2.addClass("active");
             $day1.removeClass("active");
+            $desc.hide();
         });
 
         var titles = jQuery(".page-node-174 .field-name-field-agenda-item-session-title");
@@ -24,36 +26,100 @@
             }
         });
         jQuery(" .page-node-174 .field-name-field-agenda-session-description").hide();
+
+        var ss_block =$('#views_slideshow_cycle_teaser_section_agenda-block_1');
+        var div_height = ss_block.height();
         jQuery(".accord img").click(function () {
             var move = this.parentNode.parentNode.nextSibling;
-            $(move).slideToggle(400);
-        });
-        /*var offset = $('#agenda').offset();
-        var scrollto = offset.top - 76; // minus fixed header height
-        $('html, body').animate({scrollTop:scrollto}, 0);
-        var offset1 = $('#why-attend').offset();
-        var scrollto1 = offset1.top + 76; // minus fixed header height
-        $('html, body').animate({scrollTop:scrollto1}, 0);
-        var offset2 = $('#attendees').offset();
-        var scrollto2 = offset2.top + 76; // minus fixed header height
-        $('html, body').animate({scrollTop:scrollto2}, 0);
-        var offset3 = $('#travel').offset();
-        var scrollto3 = offset3.top + 76; // minus fixed header height
-        $('html, body').animate({scrollTop:scrollto3}, 0);
+            $(move).slideToggle(400, function () {
+                var seen = $(".field-name-field-agenda-session-description:visible").length;
+                ss_block.css('max-height',div_height + 130 * seen);
+            });
 
-        $(function() {
-            setTimeout(delayedFragmentTargetOffset, 500);
         });
 
-// add scroll offset to fragment target (if there is one)
-        function delayedFragmentTargetOffset(){
-            var offset = $(':target').offset();
-            if(offset){
-                var scrollto = offset.top - 76; // minus fixed header height
-                $('html, body').animate({scrollTop:scrollto}, 0);
-            }
-        }*/
 
+
+
+        (function(document, history, location) {
+            var HISTORY_SUPPORT = !!(history && history.pushState);
+
+            var anchorScrolls = {
+                ANCHOR_REGEX: /^#[^ ]+$/,
+                OFFSET_HEIGHT_PX: 76,
+
+                /**
+                 * Establish events, and fix initial scroll position if a hash is provided.
+                 */
+                init: function() {
+                    this.scrollToCurrent();
+                    window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+                    document.body.addEventListener('click', this.delegateAnchors.bind(this));
+                },
+
+                /**
+                 * Return the offset amount to deduct from the normal scroll position.
+                 * Modify as appropriate to allow for dynamic calculations
+                 */
+                getFixedOffset: function() {
+                    return this.OFFSET_HEIGHT_PX;
+                },
+
+                /**
+                 * If the provided href is an anchor which resolves to an element on the
+                 * page, scroll to it.
+                 * @param  {String} href
+                 * @return {Boolean} - Was the href an anchor.
+                 */
+                scrollIfAnchor: function(href, pushToHistory) {
+                    var match, rect, anchorOffset;
+
+                    if(!this.ANCHOR_REGEX.test(href)) {
+                        return false;
+                    }
+
+                    match = document.getElementById(href.slice(1));
+
+                    if(match) {
+                        rect = match.getBoundingClientRect();
+                        anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+                        window.scrollTo(window.pageXOffset, anchorOffset);
+
+                        // Add the state to history as-per normal anchor links
+                        if(HISTORY_SUPPORT && pushToHistory) {
+                            history.pushState({}, document.title, location.pathname + href);
+                        }
+                    }
+
+                    return !!match;
+                },
+
+                /**
+                 * Attempt to scroll to the current location's hash.
+                 */
+                scrollToCurrent: function() {
+                    this.scrollIfAnchor(window.location.hash);
+                },
+
+                /**
+                 * If the click event's target was an anchor, fix the scroll position.
+                 */
+                delegateAnchors: function(e) {
+                    var elem = e.target;
+
+                    if(
+                        elem.nodeName === 'A' &&
+                        this.scrollIfAnchor(elem.getAttribute('href'), true)
+                    ) {
+                        e.preventDefault();
+                    }
+                }
+            };
+
+            window.addEventListener(
+                'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+            );
+        })(window.document, window.history, window.location);
 
     });
 
